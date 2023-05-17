715cc45fc98c2db38d9bd9ea62eff63bfe57d760
Mark Obom <mark.obom@sky.uk>
2023-05-17 12:40:54 +0100

    Created EmployeeRepo and custom exception class

---
+++ src/main/java/com/SpringBoot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.SpringBoot.repository;
+
+import com.SpringBoot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
